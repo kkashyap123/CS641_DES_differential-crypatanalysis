54f0ac78e331608908ed4d5e17c78e2b66532090
kkashyap123 <32977442+kkashyap123@users.noreply.github.com>
2019-02-27 18:09:52 +0530

    added function for inverse of inital permutation

--- assignment4.py
+++ assignment4.py
@@ -2,6 +2,9 @@
 def padhexa(s):
     return '0x' + s[2:].zfill(16)
 
+def padbin(s):
+	return '0b'+s[2:].zfill(64)
+
 def hex2str(str):
 	padhexa(str)
 	lst=[]
@@ -17,28 +20,41 @@ def output(outp):
 		lst.append(hex(ord(outp[i])-ord('f'))[2])
 	return ''.join(lst)
 
-	
+def inv_ip(s):
+	s=padbin(s)
+	print(s)
+	s=s[2:]
+
+	lst=[]
+	perm=[58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7]
+	for i in range(1,65):
+		ind=perm.index(i)
+		lst.append(s[ind])
+	s='0b'+''.join(lst)
+	return int(s,0)
+
 import random, subprocess, string
 inlst1=[]
 inlst2=[]
 outlst1=[]
 outlst2=[]
-char_xor= int('0x0080820060000000',0)
+
+init_xor= int('0x0080820060000000',0)
+char_xor=inv_ip(bin(init_xor))
+
 for i in range(1):
 	in1=''.join(random.choice(string.ascii_lowercase) for i in range(8))
 	print(in1)
 	if in1 not in inlst1:
 		inlst1.append(in1)
-		print(in1.encode('utf-8').hex())
 		in2=int(in1.encode('utf-8').hex(),16)^char_xor
-		print(hex(in2))
 		in2=hex2str(hex(in2))
-		
+		print(in2)
 		inlst2.append(in2)
 	
 
 	args1="curl -H \"Content-Type: application/json\" --request POST --data \'{\"teamname\":\"team5\",\"password\":\"ayz11ospfc\", \"plaintext\":\""+in1+"\"}\' -k https://172.27.26.163:9000/des"
-	args2="curl -H \"Content-Type: application/json\" --request POST --data \'{\"teamname\":\"team5\",\"password\":\"ayz11ospfc\", \"plaintext\":\""+"password"+"\"}\' -k https://172.27.26.163:9000/des"
+	args2="curl -H \"Content-Type: application/json\" --request POST --data \'{\"teamname\":\"team5\",\"password\":\"ayz11ospfc\", \"plaintext\":\""+in2+"\"}\' -k https://172.27.26.163:9000/des"
 	out1=subprocess.check_output(args1,shell=True)
 	out1=out1.decode('utf-8').split('"')[3]
 	print(out1)
@@ -48,7 +64,5 @@ for i in range(1):
 	out2=out2.decode('utf-8').split('"')[3]
 	print(out2)
 	outlst2.append(int(output(out2),0))
-
-
 
 
